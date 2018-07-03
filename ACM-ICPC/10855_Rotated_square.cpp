7365a44f6d94921e4ff8a6e21ab7a7fc09d0b657
kartikgiri <kartikgiri92@gmail.com>
2018-07-03 20:47:05 +0530

    Changes in ROtated Squares file

--- ACM-ICPC/10855_Rotated_square.cpp
+++ ACM-ICPC/10855_Rotated_square.cpp
@@ -8,8 +8,7 @@ using namespace std;
 int n,N;
 char X[500][500], Y[500][500], Z[500][500];
 
-void rotate()       //function to rotate small square
-{
+void rotate(){       //function to rotate small square
     frb(i,0,n)
         frb(j,0,n)
             Z[j][n-1-i] = Y[i][j];
@@ -18,8 +17,7 @@ void rotate()       //function to rotate small square
             Y[i][j] = Z[i][j];
 }
 
-bool same(int p, int q) 
-{
+bool same(int p, int q){
     frb(i,0,n)
         frb(j,0,n)
             if(Y[i][j] != X[i + p][j + q])
@@ -28,8 +26,7 @@ bool same(int p, int q)
     return(true);
 }
 
-int match()
-{
+int match(){
     int temp = 0;
     frb(i,0,N-n+1)                  //from each table index the small square will be checked
         frb(j,0,N-n+1)
@@ -40,16 +37,13 @@ int match()
 }
 
 
-int main()
-{
-    while(scanf("%d %d",&N,&n),(N || n))
-    {
+int main(){
+    while(scanf("%d %d",&N,&n),(N || n)){
         frb(i,0,N)
             scanf("%s",&X[i]);
         frb(i,0,n)
             scanf("%s",&Y[i]);
-        frb(i,0,4)
-        {
+        frb(i,0,4){
             if(i)  rotate();
             printf("%d%c",match(),i < 3 ? ' ' : '\n');
         }
